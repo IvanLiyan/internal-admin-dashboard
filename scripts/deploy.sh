2b10173fff491e4f74dfb3c77e416dc887e7fb27
Yifan Zhang <yifzhang@wish.com>
2023-04-25 13:54:46 -0400

    add deployment scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+for i in "$@"
+do
+case $i in
+    -s=*|--s3_bucket=*)
+    S3_BUCKET="${i#*=}"
+    shift # past argument=value
+    ;;
+    *) # unknown option
+    ;;
+esac
+done
+
+echo "Checking AWS configuration"
+aws configure list
+
+echo "Copy non-html non-js build contents to S3"
+aws s3 sync --exclude "*.html" --exclude "*.js" ./out s3://$S3_BUCKET/internal-admin
+
+echo "Copy html build contents to S3"
+for f in $(find out -name '*.js')
+do
+#  echo "Encoding $f"
+#  brotli $f
+ cleaned=${f#out/}
+ aws s3 cp $f s3://$S3_BUCKET/internal-admin/$cleaned --content-type "application/javascript"
+done
+
+echo "Copy html build contents to S3"
+for f in $(find out -name '*.html')
+do
+ tmp=${f#out/}
+ cleaned=${tmp%.html}
+ aws s3 cp $f s3://$S3_BUCKET/internal-admin/$cleaned --content-type "text/html" --cache-control "no-cache"
+done
