4df5c1ae2bb391d4daa7cbfe1cfe1674f1a3d8c7
Yifan Zhang <yifzhang@wish.com>
2023-05-01 16:15:37 -0400

    s3 sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,15 +14,16 @@ echo "Checking AWS configuration"
 aws configure list
 
 echo "Copy non-html non-js build contents to S3"
-aws s3 sync --exclude "*.html" --exclude "*.js" ./out s3://$S3_BUCKET/internal-admin
+aws s3 sync --delete --exclude "*.html" --exclude "*.js" ./out s3://$S3_BUCKET/internal-admin
+
+echo "Compressing JS"
+find out -name '*.js' | xargs brotli -vf
 
 echo "Copy html build contents to S3"
 for f in $(find out -name '*.js')
 do
-#  echo "Encoding $f"
-#  brotli $f
  cleaned=${f#out/}
- aws s3 cp $f s3://$S3_BUCKET/internal-admin/$cleaned --content-type "application/javascript"
+ aws s3 cp $f.br s3://$S3_BUCKET/internal-admin/$cleaned --content-type "application/javascript" --content-encoding "br"
 done
 
 echo "Copy html build contents to S3"
