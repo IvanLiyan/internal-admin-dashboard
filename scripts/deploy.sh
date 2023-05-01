73c53b7120f1b1c6588573fd38afeb9019202f7f
Yifan Zhang <yifzhang@wish.com>
2023-05-01 14:32:14 -0400

    wget

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ esac
 done
 
 echo "Installing AWS CLI"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 
